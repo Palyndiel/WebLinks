bb8df295b378aa7e34e83a38ddf97f41d78787a2
Palyndiel <maxym.mino@gmail.com>
2015-11-15 11:49:50 +0100

    maj de la partie controleur : routes et app

--- app/app.php
+++ app/app.php
@@ -13,6 +13,20 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+    'security.firewalls' => array(
+        'secured' => array(
+            'pattern' => '^/',
+            'anonymous' => true,
+            'logout' => true,
+            'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
+            'users' => $app->share(function () use ($app) {
+                return new MicroCMS\DAO\UserDAO($app['db']);
+            }),
+        ),
+    ),
+));
 
 // Register services
 $app['dao.link'] = $app->share(function ($app) {
